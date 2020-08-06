953a38e8a5de46de022b8f94b93db43dafc723c7
nikmargit <nikola.margit@gmail.com>
2020-08-06 17:49:22 +0200

    Updates

---
+++ precache-manifest.70ba36ff061090caa7bbe4c75c6aa09a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8f37efbba68412c2247dd165576df1",
+    "url": "/qr-on-pdf/index.html"
+  },
+  {
+    "revision": "5201cd8ac558bd2e2e25",
+    "url": "/qr-on-pdf/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "be93e271654dca2d5fea",
+    "url": "/qr-on-pdf/static/css/main.6c46d1b9.chunk.css"
+  },
+  {
+    "revision": "5201cd8ac558bd2e2e25",
+    "url": "/qr-on-pdf/static/js/2.2e86732f.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/qr-on-pdf/static/js/2.2e86732f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be93e271654dca2d5fea",
+    "url": "/qr-on-pdf/static/js/main.15f4a387.chunk.js"
+  },
+  {
+    "revision": "e789874c33233ce0364c",
+    "url": "/qr-on-pdf/static/js/runtime-main.d9b57fb8.js"
+  }
+]);
